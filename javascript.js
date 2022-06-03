9b322d60017b5a8f32dec61a1df2c69ea7cb5c9a
Femi  Oladapo <femioladapo@Femis-MacBook-Air.local>
2022-06-03 23:13:12 +0100

    add files to project

---
+++ javascript.js
@@ -0,0 +1,5 @@
+    alert("Welcome!");
+let message;
+let name = prompt("Please enter your name:");
+  message = alert("Welcome " + name + "! How are you today?");
+document.getElementById("demo").innerHTML = text;
