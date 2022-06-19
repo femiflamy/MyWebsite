3b8b12c91da896a3a05f341f4ed0e91639b88865
Femi  Oladapo <femioladapo@Femis-MacBook-Air.local>
2022-06-19 12:36:09 +0100

    19/06/2022

--- javascript.js
+++
@@ -1,5 +0,0 @@
-    alert("Welcome!");
-let message;
-let name = prompt("Please enter your name:");
-  message = alert("Welcome " + name + "! How are you today?");
-document.getElementById("demo").innerHTML = text;
